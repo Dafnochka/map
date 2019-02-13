f6b4f5c50541f86982055117490a6ed0834c9b73
Dafna <39526021+Dafnochka@users.noreply.github.com>
2019-02-13 11:35:41 +0300

    Страница приложения

---
+++ precache-manifest.527d928dc0db1ca2be613b18ae2b4c72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "32daac36a71219cda26c0d38ca542cbb",
+    "url": "/static/media/sova.32daac36.png"
+  },
+  {
+    "revision": "92da0fbd6b2efdcd67c705854594a8f0",
+    "url": "/static/media/bull.92da0fbd.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75d70e6586bd25e2155b",
+    "url": "/static/js/main.75d70e65.chunk.js"
+  },
+  {
+    "revision": "8a2fdeb77934ccf3d759",
+    "url": "/static/js/1.8a2fdeb7.chunk.js"
+  },
+  {
+    "revision": "75d70e6586bd25e2155b",
+    "url": "/static/css/main.80b23c5f.chunk.css"
+  },
+  {
+    "revision": "67ad142bf52844a02f207dbd4a4c1a0c",
+    "url": "/index.html"
+  }
+];
